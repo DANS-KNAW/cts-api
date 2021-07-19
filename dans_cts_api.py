ecd68f7fab8ed7645cb629e6e6ccb9caa8e9a6b5
Vesa Akerman <vesa.akerman@dans.knaw.nl>
2021-07-19 15:37:54 +0200

    version without MongoDb

--- dans_cts_api.py
+++ dans_cts_api.py
@@ -1,13 +1,17 @@
 import xmltodict
-from bottle import route, run, request, abort, response
-from pymongo import MongoClient
+from bottle import route, run, request, response
 import requests
+import csv
+import re
+from xml.etree.ElementTree import Element, SubElement, tostring
+from xml.dom.minidom import parseString
 
 RE3DATA_API_REPOSITORIES = "https://www.re3data.org/api/beta/repositories"
+RE3DATA_API_REPOSITORY = "https://www.re3data.org/api/v1/repository/{}"
 RE3DATA_API_REPOSITORIES_CERTIFICATE = "https://www.re3data.org/api/beta/repositories?query=&certificates[]={}"
+CERTIFICATE_PATTERN = ".*(<r3d:certificate>other</r3d:certificate>).*"
 
-connection = MongoClient('localhost', 27017)
-db = connection.dans_cts
+certificates = {}
 
 
 @route('/repositories', method='GET')
@@ -32,32 +36,61 @@ def get_repository(id):
     if format != "" and format != "xml" and format != "json":
         return "Wrong format"
 
-    entity = db['repositories'].find_one({'_id': id})
-    if not entity:
-        abort(404, 'No document with id %s' % id)
-
+    re3_data = requests.get(RE3DATA_API_REPOSITORY.format(id)).text
+    data = enrich(re3_data, id) if certificates.get(id) else re3_data
     if format == "json":
         response.content_type = "application/json"
-        data = xmltodict.parse(entity["r3d:re3data"])
+        return xmltodict.parse(data)
     else:
         response.content_type = "text/xml"
-        data = entity["r3d:re3data"]
-    return data
+        return data
 
 
 def get_parameter_value(s, parameter):
-    ind = len(parameter) + 1 if s.startswith(parameter + "=") else s.find("&" + parameter + "=") + len(parameter) + 2
-    if ind >= len(parameter) + 1:
-        start = s[ind:]
-        end_ind = start.find("&")
-        if end_ind >= 0:
-            value = start[:end_ind]
+    if s.find(parameter + "=") >= 0:
+        ind = len(parameter) + 1 if s.startswith(parameter + "=") else s.find("&" + parameter + "=") + len(parameter) + 2
+        if ind >= len(parameter) + 1:
+            start = s[ind:]
+            end_ind = start.find("&")
+            if end_ind >= 0:
+                value = start[:end_ind]
+            else:
+                value = start
         else:
-            value = start
+            value = ""
     else:
         value = ""
     return value
 
 
+def get_certification_data(cts_info):
+    global certificates
+
+    with open(cts_info) as csv_input:
+        csv_reader = csv.reader(csv_input, delimiter=';')
+        next(csv_reader, None)  # skip the headers
+        for row in csv_reader:
+            certificates[row[0]] = [row[2], row[3], row[4]]
+
+
+def enrich(re3_xml, repository_id):
+    r = re.match(CERTIFICATE_PATTERN, re3_xml.replace('\n', ''))
+    if r:
+        re3data_certificate = r.group(1)
+        return re3_xml.replace(re3data_certificate, "%s" % make_certificate_node(repository_id), 1)
+    else:
+        return re3_xml
+
+
+def make_certificate_node(repository_id):
+    certificate = Element("certificate")
+    SubElement(certificate, "certificateName").text = "CoreTrustSeal"
+    SubElement(certificate, "startDate").text = certificates[repository_id][0]
+    SubElement(certificate, "endDate").text = certificates[repository_id][1]
+    SubElement(certificate, "certificationType").text = certificates[repository_id][2]
+    certificate_xml = parseString(tostring(certificate, "utf-8")).childNodes[0].toprettyxml()
+    return certificate_xml
+
+get_certification_data("CTS_INFO.csv")
 run(host='localhost', port=8080)
 
